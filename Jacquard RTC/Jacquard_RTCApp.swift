e42fff880aab5d67ba12a2693f83bf56b6049588
Phumrapee Limpianchop <git@rayriffy.com>
2022-08-02 04:13:50 +0900

    Initial Commit

---
+++ Jacquard RTC/Jacquard_RTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jacquard_RTCApp.swift
+//  Jacquard RTC
+//
+//  Created by Phumrapee Limpianchop on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct Jacquard_RTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
