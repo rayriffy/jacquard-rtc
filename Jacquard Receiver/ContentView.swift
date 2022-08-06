e08c2c5b40a3d61a14d8a835b5c471f616fa83a0
Phumrapee Limpianchop <git@rayriffy.com>
2022-08-07 02:49:13 +0900

    feat: staged changes macOS

--- Jacquard Receiver/ContentView.swift
+++ Jacquard Receiver/ContentView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+import MultipeerKit
 
 struct ContentView: View {
+  let transceiver = MultipeerTransceiver(configuration: MultipeerConfiguration(
+    serviceType: "JacquardRTC",
+    peerName: "Receiver - macOS",
+    defaults: .standard,
+    security: .default,
+    invitation: .automatic
+  ))
+
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello, world!")
+      Form {
+//        KeyboardShortcuts.Recorder("Brush Out", name: .brushOut)
+//        KeyboardShortcuts.Recorder("Brush In", name: .brushIn)
+//        KeyboardShortcuts.Recorder("Double Tap", name: .doubleTap)
+//        KeyboardShortcuts.Recorder("Cover", name: .cover)
+      }
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .task {
+      transceiver.resume()
+      
+//      transceiver.receive(TransmitterPayload.self) { payload, sender in
+//        print("recieved \(payload.gesture) from (\(sender.name))[\(sender.id)]")
+//      }
     }
   }
 }
