0983f20edb491f43b5eea6703aa8219dc4879039
Phumrapee Limpianchop <git@rayriffy.com>
2022-08-07 19:24:04 +0900

    feat(receiver): more keyboard key

--- Jacquard Receiver/Views/KeyboardPicker.swift
+++ Jacquard Receiver/Views/KeyboardPicker.swift
@@ -47,21 +47,58 @@ struct KeyboardPicker: View {
           Text("X").tag(KeyboardKey.key_x)
           Text("Y").tag(KeyboardKey.key_y)
           Text("Z").tag(KeyboardKey.key_z)
+          Divider()
+        }
+        Group {
+          Text("`").tag(KeyboardKey.key_grave)
+          Text("1").tag(KeyboardKey.key_1)
+          Text("2").tag(KeyboardKey.key_2)
+          Text("3").tag(KeyboardKey.key_3)
+          Text("4").tag(KeyboardKey.key_4)
+          Text("5").tag(KeyboardKey.key_5)
+          Text("6").tag(KeyboardKey.key_6)
+          Text("7").tag(KeyboardKey.key_7)
+          Text("8").tag(KeyboardKey.key_8)
+          Text("9").tag(KeyboardKey.key_9)
+        }
+        Group {
+          Text("0").tag(KeyboardKey.key_0)
+          Text("-").tag(KeyboardKey.key_dash)
+          Text("=").tag(KeyboardKey.key_equal)
+          Divider()
+        }
+        Group {
+          Text("[").tag(KeyboardKey.key_bracketOpen)
+          Text("]").tag(KeyboardKey.key_bracketClose)
+          Text(";").tag(KeyboardKey.key_semiColon)
+          Text("'").tag(KeyboardKey.key_singleQuote)
+          Text(",").tag(KeyboardKey.key_comma)
+          Text(".").tag(KeyboardKey.key_dot)
+          Text("/").tag(KeyboardKey.key_slash)
+          Text("\\").tag(KeyboardKey.key_backSlash)
+          Divider()
         }
         Group {
           Text("←").tag(KeyboardKey.key_arrowLeft)
           Text("→").tag(KeyboardKey.key_arrowRight)
           Text("↑").tag(KeyboardKey.key_arrowUp)
           Text("↓").tag(KeyboardKey.key_arrowDown)
-          Text("↵").tag(KeyboardKey.key_return)
-          Text("⌫").tag(KeyboardKey.key_backSpace)
-          Text("PgUp").tag(KeyboardKey.key_pageUp)
-          Text("PgDown").tag(KeyboardKey.key_pageDown)
-          Text("End").tag(KeyboardKey.key_end)
-          Text("Home").tag(KeyboardKey.key_home)
+          Divider()
         }
         Group {
+          Text("↵").tag(KeyboardKey.key_return)
+          Text("⌫").tag(KeyboardKey.key_backSpace)
           Text("␣").tag(KeyboardKey.key_space)
+          Divider()
+        }
+        Group {
+          Text("PgUp").tag(KeyboardKey.key_pageUp)
+          Text("PgDown").tag(KeyboardKey.key_pageDown)
+          Text("Home").tag(KeyboardKey.key_home)
+          Text("End").tag(KeyboardKey.key_end)
+          Divider()
+        }
+        Group {
           Text("Esc").tag(KeyboardKey.key_esc)
           Text("Del").tag(KeyboardKey.key_delete)
         }
